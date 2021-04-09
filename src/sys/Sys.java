7a2e9ed447111ae555e1cf0f43e31e6c3af2cbaf
madelinben <ben.madelin@ntlworld.com>
2021-04-09 02:46:44 +0100

    Formatted main system menu for application.

---
+++ src/sys/Sys.java
@@ -0,0 +1,36 @@
+package sys;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Sys {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        displayMenu();
+    }
+
+    public static void displayMenu() throws java.lang.Exception{
+        boolean terminate = false;
+        while (!terminate) {
+            System.out.println("Main Menu:\nQ - Quit");
+            String operation = new Scanner(System.in).nextLine().trim().toLowerCase();
+            char[] input = operation.toCharArray();
+            if(input.length != 1){
+                System.out.println("Please only enter 1 character to select a menu item.");
+                displayMenu();
+            }
+            switch (input[0]) {
+                case 'q':
+                    scanner.close();
+                    terminate = true;
+                    break;
+                default:
+                    System.out.println("ERROR! User didn't select valid input case!");
+                    break;
+            }
+        }
+        System.exit(0);
+    }
+}
